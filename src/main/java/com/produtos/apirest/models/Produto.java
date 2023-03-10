29c92fe7a3000ebfa06ced8857b56a7796b4afd7
italo <italo.santos.059@ufrn.edu.br>
2023-03-10 11:37:44 -0300

    generate getters and setters

--- src/main/java/com/produtos/apirest/models/Produto.java
+++ src/main/java/com/produtos/apirest/models/Produto.java
@@ -1,5 +1,60 @@
 package com.produtos.apirest.models;
 
-public class Produto {
+import java.io.Serializable;
+import java.math.BigDecimal;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="TB_PRODUTO")
+public class Produto implements Serializable{
+	
+	private static final long serialVersionUID = 1l;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	private String nome;
+	
+	private BigDecimal quantidade;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(BigDecimal quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	private BigDecimal valor;
+	
 }
