7c76807ab520db740ef4d4aba942f54bec1b30c3
italo <italogithub@gmail.com>
2023-03-12 00:53:49 -0300

    add nutricionista resource

--- src/main/java/com/nutrition/resource/model/NutricionistaResource.java
+++ src/main/java/com/nutrition/resource/model/NutricionistaResource.java
@@ -1,5 +1,33 @@
 package com.nutrition.resource.model;
 
-public class NutricionistaResource {
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+public class NutricionistaResource {
+	
+	
+	@JsonProperty("nome_nutricionista")
+	private String nome;
+	
+	@JsonProperty("id_paciente")
+	private String idPaciente;
+	
+	
+	public NutricionistaResource(String nome, String idPaciente) {
+		this.nome = nome;
+		this.idPaciente = idPaciente;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getIdPaciente() {
+		return idPaciente;
+	}
+	public void setIdPaciente(String idPaciente) {
+		this.idPaciente = idPaciente;
+	}
+	
+	
 }
