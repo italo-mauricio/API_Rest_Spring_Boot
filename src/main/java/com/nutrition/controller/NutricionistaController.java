a8bade7fb09c834923402775a98645573a3e5f7a
italo <italogithub@gmail.com>
2023-03-12 00:20:24 -0300

    update

--- src/main/java/com/nutrition/controller/NutricionistaController.java
+++ src/main/java/com/nutrition/controller/NutricionistaController.java
@@ -1,5 +1,11 @@
 package com.nutrition.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class NutricionistaController {
 
+	@GetMapping(path = "api/nutricionista-id/{}")
+	public void buscarNutricionistaPorId() {
+		
+	}
 }
