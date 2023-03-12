abcc0c208320f4619badbad1340b74e7a89d9a1e
italo <italogithub@gmail.com>
2023-03-12 00:32:13 -0300

    add cadastro nutricionista

--- src/main/java/com/nutrition/service/CadastroNutricionista.java
+++ src/main/java/com/nutrition/service/CadastroNutricionista.java
@@ -1,5 +1,16 @@
 package com.nutrition.service;
 
-public class CadastroNutricionista {
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.nutrition.datasource.models.Nutricionista;
+import com.nutrition.repository.NutricionistaRepository;
+
+public class CadastroNutricionista {
+	@Autowired
+	private NutricionistaRepository nutricionistaRepository;
+	
+	public void cadastro(Nutricionista nutricionista) {
+		nutricionistaRepository.saveAndFlush(nutricionista);
+	}
 }
--- src/main/java/com/nutrition/service/testes.java
+++
@@ -1,8 +0,0 @@
-package com.nutrition.service;
-
-public class testes {
-	private void psvm() {
-		// TODO Auto-generated method stub
-		System.out.println("oi");
-	}
-}
